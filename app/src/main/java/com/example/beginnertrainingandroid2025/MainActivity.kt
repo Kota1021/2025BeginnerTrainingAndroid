04b8ba5f153f109bb4feac3f3ced76f16f4e3123
Kota1021 <ko_matsumoto@yumemi.co.jp>
2025-11-09 07:39:57 +0900

    add cell

--- app/src/main/java/com/example/beginnertrainingandroid2025/MainActivity.kt
+++ app/src/main/java/com/example/beginnertrainingandroid2025/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.example.beginnertrainingandroid2025.ui.theme.BeginnerTrainingAndroid2025Theme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +46,53 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
+@Composable
+fun RepoListItem(
+    repo: Repo,
+    modifier: Modifier = Modifier,
+) {
+    Column {
+        Text(repo.name)
+        repo.description?.let { Text(it) }
+        Row {
+            Icon(
+                imageVector = Icons.Outlined.Star,
+                tint = Color.LightGray,
+                contentDescription = null,
+            )
+            Text("${repo.stars}")
+        }
+    }
+}
+
+data class Repo(
+    val id: Int,
+    val name: String,
+    val description: String? = null,
+    val stars: Int,
+)
+
+class RepoPreviewParameterProvider: PreviewParameterProvider<Repo> {
+    override val values = sequenceOf(
+        Repo(
+            id = 1,
+            name = "hoge",
+            stars = 123,
+        ),
+        Repo(
+            id = 2,
+            name = "foo",
+            description = "This is awesome repository.",
+            stars = 1234,
+        ),
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BeginnerTrainingAndroid2025Theme {
-        Greeting("Android")
-    }
+private fun RepoListItemPreview(
+    @PreviewParameter(RepoPreviewParameterProvider::class) repo: Repo,
+) {
+    RepoListItem(repo = repo)
 }
